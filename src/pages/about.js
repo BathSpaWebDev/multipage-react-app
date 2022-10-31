db65a53a86184f7de78fcfdee684d09bdb209130
OlgitH <berman.oj@gmail.com>
2022-10-31 10:53:55 +0000

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+
+function About() {
+  return (
+    <div className="About">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident placeat consequatur pariatur commodi labore voluptatem dolor mollitia, eum esse! Adipisci dicta dolores impedit dolorem, eveniet molestias corporis cumque atque porro.
+      </p>
+    </div>
+  );
+}
+
+export default About;
