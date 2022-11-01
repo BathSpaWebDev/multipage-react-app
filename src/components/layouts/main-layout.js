aa622c8e02c830a3aeb46d6e36e4780cfc39d291
OlgitH <berman.oj@gmail.com>
2022-11-01 14:13:48 +0000

    ~

--- src/components/layouts/main-layout.js
+++ src/components/layouts/main-layout.js
@@ -8,6 +8,7 @@ const MainLayout = () => {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="about">About</a></li>
+            <li><a href="products">Products</a></li>
             <li><a href="contact">Contact</a></li>
             <li><a href="dashboard">Dashboard</a></li>
           </ul>
