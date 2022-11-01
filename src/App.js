223dfe5da31465eb5602ce7663200b30f802867b
OlgitH <berman.oj@gmail.com>
2022-11-01 10:25:19 +0000

    added dashboard route

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import MainLayout from './components/layouts/main-layout';
+import DashboardLayout from './components/layouts/dashboard-layout';
+import Help from './pages/dashboard/help';
+import FAQ from './pages/dashboard/faq';
+import DashboardHome from './pages/dashboard/home';
 
 function App() {
   return (
     <div className="App">
       
-     
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<MainLayout />}>
@@ -18,10 +21,15 @@ function App() {
                   <Route path="about" element={<About />} />
                   <Route path="contact" element={<Contact />} />
               </Route>
+              <Route path="dashboard" element={<DashboardLayout />}>
+                  <Route index element={<DashboardHome />} />
+                  <Route path="help" element={<Help />}>
+                    <Route path="faq" element={<FAQ />} />
+                  </Route>
+              </Route>
             </Routes>
           </BrowserRouter>
     
-      
     </div>
   );
 }
