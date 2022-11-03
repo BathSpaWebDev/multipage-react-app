f72c9cb20bf226ffc39b9c8c34b909e5ba416854
OlgitH <berman.oj@gmail.com>
2022-11-03 11:03:39 +0000

    ~

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,45 @@
+import {useState} from 'react';
+import Message from '../components/message';
 
 function About() {
+  const initialText = `Tuscola, Neb. – The Nebraska baseball team (6-3) improved to 2-1 on its current season-opening road trip, earning a 5-4 victory over host Tuscola (4-2) on`;
+  // const [text, setText] = useState(initialText);
+
+  const person = {
+    name: 'Sally',
+    age: 21
+  }
+
+  const [myList, setMyList] = useState(['Tea','Coffee','Hot chocolate']);
+  const [currentPerson, setCurrentPerson] = useState(person);
+  const [currentColour, setCurrentColour] = useState('blue');
+
+
+  function clickHandler(){
+    setMyList( [...myList, 'Coca cola'] )
+    setCurrentPerson({...currentPerson, name: 'Tamara', age: '44'});
+    setCurrentColour('green')
+    // setText('something else');
+  }
+
   return (
     <div className="About">
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident placeat consequatur pariatur commodi labore voluptatem dolor mollitia, eum esse! Adipisci dicta dolores impedit dolorem, eveniet molestias corporis cumque atque porro.
-      </p>
+
+      <Message messageColour={currentColour} textColour="white">
+        <h1>The current person is {currentPerson.name}, with the age of {currentPerson.age}</h1>
+      </Message>
+
+      {/* <h1>The current person is {currentPerson.name}, with the age of {currentPerson.age}</h1> */}
+      
+      {
+        myList.length > 0 ?
+        <ul>
+          {myList.map((item, index) => <li key={index}>{item}</li>)}
+        </ul> 
+        :
+        ''
+      }
+      <button onClick={clickHandler}>Change everything</button>
     </div>
   );
 }
